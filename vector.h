c6ac76c49baffbb5c1724132e4d75637868e3848
Cluley <oo43@yandex.ru>
2023-05-27 20:18:39 +0700

    minor betterments

--- vector.h
+++ vector.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 namespace custom {
 
 	template<class T>
@@ -8,13 +10,13 @@ namespace custom {
 		vector() : size_(1), capacity_(1) {
 			array = new T[capacity_]();
 		}
-		vector(const unsigned size) : size_(size), capacity_(size) {
+		vector(const size_t size) : size_(size), capacity_(size) {
 			array = new T[capacity_]();
 		}
 		vector(const vector& other) : size_(other.size_), capacity_(other.capacity_) {
 			array = new T[capacity_]();
 
-			for (unsigned i = 0; i < size_; ++i)
+			for (size_t i = 0; i < size_; ++i)
 			{
 				this->array[i] = other->array[i];
 			}
@@ -33,19 +35,19 @@ namespace custom {
 			delete[] array;
 			array = new T[capacity_]();
 
-			for (unsigned i = 0; i < size_; ++i)
+			for (size_t i = 0; i < size_; ++i)
 			{
 				this->array[i] = other->array[i];
 			}
 
 			return this;
 		}
-		T at(unsigned idx) const {
+		T at(size_t idx) const {
 			if (idx >= size_) throw std::out_of_range("What is better: to be born good or to overcome your evil nature through great effort?");
 
 			return array[idx];
 		}
-		T& at(unsigned idx) {
+		T& at(size_t idx) {
 			if (idx >= size_) throw std::out_of_range("What is better: to be born good or to overcome your evil nature through great effort?");
 			
 			return array[idx];
@@ -53,30 +55,30 @@ namespace custom {
 		void push_back(const T el_to_push) {
 			if (size_ == capacity_)
 			{
-				capacity_ = static_cast<unsigned>(capacity_ * 1.5);
+				capacity_ = static_cast<size_t>(capacity_ * 1.5);
 
 #pragma warning(push)
 #pragma warning(disable:6386)
 
 				T* temp_array = new T[capacity_]();
-				for (unsigned i = 0; i < size_; ++i)
+				for (size_t i = 0; i < size_; ++i)
 				{
 					temp_array[i] = array[i];
 				}
 
 				delete[] array;
-				array = temp_array;
+				array = std::move(temp_array);
 			}
 			array[size_] = el_to_push;
 			++size_;
 
 #pragma warning(pop)
 		}
-		unsigned size() const { return size_; }
-		unsigned capacity() const { return capacity_; }
+		size_t size() const { return size_; }
+		size_t capacity() const { return capacity_; }
 	private:
 		T* array;
-		unsigned size_;
-		unsigned capacity_;
+		size_t size_;
+		size_t capacity_;
 	};
 }
